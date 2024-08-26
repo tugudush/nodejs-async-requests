8adfd3817c6b7ad2ed2572f1e1c77531c3b8227f
tugudush <jerome2kph@gmail.com>
2024-08-26 11:40:56 +0800

    progress

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,6 @@
 const deleteProduct = (btn) => {
-  console.log(btn.parentNode.querySelector("[name=productId]").value);
+  //console.log(document.querySelector("[name=productId]").value);
+  //console.log(btn.parentNode.querySelector("[name=productId]").value);
+  const productId = btn.getAttribute('data-product-id');
+  console.log(productId);
 };
