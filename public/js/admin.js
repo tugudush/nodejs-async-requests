629a8ef7e7ca8adb391e13c68aa2579bbc179dac
tugudush <jerome2kph@gmail.com>
2024-08-26 13:23:06 +0800

    http request: delete

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,4 +5,17 @@ const deleteProduct = (btn) => {
   const prodId = btn.getAttribute("data-product-id");
   console.log("csrf: ", csrf);
   console.log("prodId: ", prodId);
+
+  fetch("/admin/product/" + prodId, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrf,
+    },
+  })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
